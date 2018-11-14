e09d3e08ae3346041b4792f08ba4027d050fbc37
root <twhtanghk@gmail.com>
2018-11-14 12:26:04 +0800

    add system owned eventBus as default value for model instances

--- frontend/src/model.vue
+++ frontend/src/model.vue
@@ -2,6 +2,7 @@
 Vue = require('vue').default
 Vue.use require('vue.oauth2/src/plugin').default
 Vue.use require('vue.model/src/plugin').default
+{eventBus} = require('./lib').default
 
 export default
   User: new Vue
@@ -9,9 +10,13 @@ export default
     props:
       baseUrl:
         default: 'api/user'
+      eventBus:
+        default: eventBus
   Cert: new Vue
     extends: Vue.component 'model'
     props:
       baseUrl:
         default: 'api/cert'
+      eventBus:
+        default: eventBus
 </script>
